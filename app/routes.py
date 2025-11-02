b866175ce042b2db14938f9ee27c69c955f31e32
Malachi-McCloud <mwmccloud2@outlook.com>
2025-11-01 22:05:56 -0400

    Initial commit: Flask portfolio scaffold with resume page

---
+++ app/routes.py
@@ -0,0 +1,36 @@
+# app.routes.py
+
+from flask import Blueprint, render_template
+
+# Create a blueprint for modular routing
+main = Blueprint('main', __name__)
+
+# Route for homepage
+@main.route('/')
+def index():
+    return render_template('index.html')
+
+# Route for resume page
+@main.route('/resume')
+def resume():
+    return render_template('resume.html')
+
+# Route for analytics page
+@main.route('/analytics')
+def analytics():
+    return render_template('analytics.html')
+
+# Route for projects page
+@main.route('/projects')
+def project():
+    return render_template('projects.html')
+
+# Route for experience page
+@main.route('/experience')
+def experience():
+    return render_template('experience.html')
+
+# Route for contact page
+@main.route('/contact')
+def contact():
+    return render_template('contact.html')
